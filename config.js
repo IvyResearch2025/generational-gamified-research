71ec54ce0bf37d21465df9f5e227e3cd7224ffa2
IvyResearch2025 <anhgiangngoc030@gmail.com>
2025-06-16 19:48:30 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SHEET_CONFIG = {
+  endpoint: "https://script.google.com/u/0/home/projects/1s5kEgTB0rZY5VUjIfgOwjRssEpAwvijoRNe0QJB3iLed_wcCxZRbOoKY/edit",
+  sheetName: "AnswerA",
+  // list các biến Storyline bạn muốn gửi
+  vars: ["consentStatus","captainsName","captainsName1","captainsOccupation","VcaptainsOccupation1","captainsYOB", "captainsYOB1","captainsEmail,"crisisChoice","reflectionNotes1","reflectionNotes2","leadershipStyles","captainsConflictChoice1","reflectionPrompts"]
